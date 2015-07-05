fae14c968ea69fc723f531354da14b2a5c1eb6da
thoppe <travis.hoppe@gmail.com>
2015-07-05 13:48:04 -0400

    Added type information to data objects.

--- buildbot_flows/flow_datatypes.py
+++ buildbot_flows/flow_datatypes.py
@@ -4,17 +4,38 @@ class generic_node_container(object):
     def __init__(self,*args,**kwargs):
         self.data = dict()
         self.data.update(self._object_defaults)
-        self.data.update(**kwargs)
+
+        # Identify the object types from the defaults
+        self.obj_types = dict([(key,type(value)) for key,value
+                            in self._object_defaults.items()])
+
+        # Map any passed values to the container
+        for key in kwargs:
+            self[key] = kwargs[key]
         
     def __getitem__(self, key):
         return self.data[key]
+    
     def __setitem__(self, key, value):
+        # Don't allow new fields to be set
+        if key not in self:
+            msg = "{} not a key in {} node, will not create new fields."
+            raise KeyError(msg.format(key,type(self)))
+        
+        # Check the type of the new value
+        if not isinstance(value, self.obj_types[key]):
+            msg = "{} is expected to be {}, passed {}"
+            raise TypeError(msg.format(key,self.obj_types[key],type(value)))
         self.data[key] = value
+
     def __repr__(self):
         return str(self.data)
 
     def keys(self):
         return self.data.keys()
+
+    def types(self):
+        return self.obj_types
 
     def get_id(self):
         print self
@@ -26,6 +47,14 @@ class generic_node_container(object):
     def json(self):
         return json.dumps(self.data, indent=2)
 
+
+
+#######################################################################
+'''
+Each node in the graphdb is a contract that the following fields will
+exist, be of the type listed, and will be populated with the default
+value if none is given.
+'''
 #######################################################################
 
 class flow(generic_node_container):
@@ -45,3 +74,4 @@ class validation(generic_node_container):
         "success"  : "",
         "failure"  : "",
     }
+
