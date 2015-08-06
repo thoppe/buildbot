6424115f69e47658c63ed53a6a6f76adaca6a800
thoppe <travis.hoppe@gmail.com>
2015-08-06 17:29:14 -0400

    API docs inital draft complete.

--- buildbot/REST_API_buildbot.py
+++ buildbot/REST_API_buildbot.py
@@ -18,20 +18,42 @@ json2edge = lambda x,**a:inter.convert_json2edge_container(x,gdb.package,**a)
 
 API_DOCS = {}
 API_DOCS["create_node"] = {
-    "method" : "POST",
-    "description" : "Create new nodes",
+    "methods" : "POST",
+    "description" : "Create a new node.",
     "url" : '/buildbot/api/v1.0/node/create',
     "labels" : gdb.package.nodes.keys()
     }
 
-API_DOCS["create_rel"] = {
-    "method" : "POST",
-    "description" : "Create relationship",
+API_DOCS["create_relationship"] = {
+    "methods" : "POST",
+    "description" : "Create a new relationship.",
     "url" : '/buildbot/api/v1.0/relationship/create',
     "labels" : ["{}-[{}]->{}".format(*x) for x in gdb.package.relationships]
 }
 
+API_DOCS["update_node"] = {
+    "methods" : "POST",
+    "description" : "Update a node.",
+    "url" : '/buildbot/api/v1.0/node/update',
+}
 
+API_DOCS["remove_node"] = {
+    "methods" : "POST",
+    "description" : "Remove a node.",
+    "url" : '/buildbot/api/v1.0/node/remove/<int:node_id>'
+}
+
+API_DOCS["remove_relationship"] = {
+    "methods" : "POST",
+    "description" : "Remove a relationship.",
+    "url" : '/buildbot/api/v1.0/relationship/remove/<int:rel_id>'
+}
+
+API_DOCS["get_node"] = {
+    "methods" : "GET",
+    "description" : "Retrieves a node by index.",
+    "url" : '/buildbot/api/v1.0/node/<int:node_id>'
+}
 
 #!flask/bin/python
 from flask import Flask, request, abort, render_template
@@ -43,10 +65,17 @@ def landing_page():
     data["package_name"]  = gdb.package.meta["package_name"]
     data["package_owner"] = gdb.package.meta["package_owner"]
 
-    data["API_DOCS"] = API_DOCS
+    # Set the order to display here!
+    key_order = ["create_node", "create_relationship",
+                 "get_node",
+                 "remove_node","remove_relationship",
+                 "update_node"
+                 ]
+    data["API_DOCS"] = [API_DOCS[key] for key in key_order]
     return render_template("apidocs.html",**data)
 
-@API.route('/buildbot/api/v1.0/relationship/create', methods=['POST'])
+doc_key = "create_relationship"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def create_relationship():
     data = request.get_json()
 
@@ -67,27 +96,30 @@ def create_relationship():
 
     return js_out, 201
 
-@API.route('/buildbot/api/v1.0/relationship/remove/<int:rel_id>',
-           methods=['POST'])
+doc_key = "remove_relationship"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def remove_relationship(rel_id):
     result = gdb.remove_relationship(rel_id)
     return json.dumps(result), 200
 
 ###########################################################################
 
-@API.route('/buildbot/api/v1.0/node/<int:node_id>', methods=['GET'])
+doc_key = "get_node"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def get_node(node_id):
     obj     = gdb[node_id]    
     node    = neo2node(obj)
     js_out  = node.json()
     return js_out, 200
 
-@API.route('/buildbot/api/v1.0/node/remove/<int:node_id>', methods=['POST'])
+doc_key = "remove_node"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def remove_node(node_id):
     result = gdb.remove_node(node_id)
     return json.dumps(result), 200
 
-@API.route('/buildbot/api/v1.0/node/create', methods=['POST'])
+doc_key = "create_node"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def create_node():
     data = request.get_json()
     
@@ -102,7 +134,9 @@ def create_node():
     js_out = node.json()
     return js_out, 201
 
-@API.route('/buildbot/api/v1.0/node/update', methods=['POST'])
+
+doc_key = "update_node"
+@API.route(API_DOCS[doc_key]["url"], methods=[API_DOCS[doc_key]["methods"]])
 def update_node():
     data = request.get_json()
     
