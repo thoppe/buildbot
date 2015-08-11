7d74696fc821ce6b011369016bc9676183d4e3f3
thoppe <travis.hoppe@gmail.com>
2015-08-11 01:06:30 -0400

    Working version up on AWS.

--- configuration/fabfile.py
+++ configuration/fabfile.py
@@ -1,9 +1,10 @@
-from fabric.api import run, env, local, sudo
+from fabric.api import run, env, local, sudo, cd
 
 config = {
     "public_DNS"  : "ec2-52-3-179-145.compute-1.amazonaws.com",
     "user"  : "ubuntu",
     "f_pam" : "buildbot_AWS.pem",
+    "github_repo" : "https://github.com/tulsa/buildbot",
 }
 
 env.hosts = [config["public_DNS"],]
@@ -15,7 +16,29 @@ def ssh():
     local(cmd.format(**config))
 
 def configure():
-    #sudo("apt-get update")
-    sudo("apt-get install -y git python-pip docker docker.io")
+    sudo("apt-get update")
+    sudo("apt-get install -y python-dev git python-pip dtach")
+    sudo("pip install PyCrypto fabric")
+
+
+    # Install docker PGP key
+    sudo("apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D")
+    sudo("echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' | sudo tee -a /etc/apt/sources.list")
     
+    sudo("apt-get update")
+    sudo("apt-get install docker-engine")
+    sudo("usermod -a -G docker ubuntu")
+    
+    run('git clone {github_repo}'.format(**config))
+    with cd("buildbot"):
+        sudo("pip install -r requirements.txt")
+        run('fab docker_neo4j')
+    
+def api():
+    print "Move this to the background"
+    with cd("buildbot"):
+        run("nohup fab api >& /dev/null < /dev/null &",pty=False)
+
+def view():
+    print "View http://{public_DNS}:5000".format(**config)
     
