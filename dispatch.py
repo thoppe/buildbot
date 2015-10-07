01cee1299ae8a307b1bb9a9c08b9a37af7beb92a
thoppe <travis.hoppe@gmail.com>
2015-10-07 17:23:20 -0400

    Dispatch pulls missing images.

--- dispatch.py
+++ dispatch.py
@@ -1,4 +1,4 @@
-import argparse, subprocess
+import argparse, subprocess, json, logging, time
 from pprint import pprint
 
 desc = '''Dispatcher for BuildBot'''
@@ -8,6 +8,22 @@ parser.add_argument('--list',
                     action='store_true',
                     help='Returns the running buildbot instances.')
 args = vars(parser.parse_args())
+logging.basicConfig(level=logging.INFO)
+
+required_containers = [
+    'tpires/neo4j',
+]
+
+def docker_pull(name):
+    args = {"name": name}
+    cmd = 'docker pull {name}'.format(**args)
+    output = subprocess.call(cmd, shell=True)
+
+def docker_inspect(name):
+    args = {"name": name}
+    cmd = 'docker inspect {name}'.format(**args)
+    output = subprocess.check_output(cmd, shell=True)
+    return json.loads(output)
 
 def docker_ps(images=False):
     '''
@@ -34,6 +50,7 @@ def docker_ps(images=False):
               if line]
     return data
 
+
 if args["list"]:
 
     print "** Running Containers **"
@@ -43,6 +60,21 @@ if args["list"]:
     print "** Running Images **"
     data = docker_ps(images=True)
     print data
+    exit(0)
+
+
+for container_name in required_containers:
+    try:
+        docker_inspect(container_name)
+        msg = "Found required container {}.".format(container_name)
+        logging.info(msg)
+    except subprocess.CalledProcessError:
+        msg = "Failed to load container {}.".format(container_name)
+        logging.error(msg)
+        msg = "Pulling image in 10 seconds if not canceled."
+        logging.warning(msg)
+        time.sleep(10)
+        docker_pull(container_name)
 
 
 
