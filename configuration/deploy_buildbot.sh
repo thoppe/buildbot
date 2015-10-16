9c9637c7854f900dc26a6860eb60556276f3246e
thoppe <travis.hoppe@gmail.com>
2015-10-16 01:14:11 -0400

    Added fab start/stop commands

--- configuration/deploy_buildbot.sh
+++ configuration/deploy_buildbot.sh
@@ -1,8 +1,17 @@
-
+# If the repo hasn't been cloned, do so now
 if [ ! -d "buildbot" ]; then
     git clone "https://github.com/tulsa/buildbot.git"
 fi
 
+# Upgrade pip
 sudo -H pip -q install pip -U
+
+# Install the requirements
 cd buildbot
 sudo -H pip -q install -r requirements.txt
+
+# Start a test instance (this downloads the docker images needed)
+fab start
+
+# Stop the test image
+fab stop
