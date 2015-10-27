08144f8361e90b087c144a250c5027204f2d07b5
thoppe <travis.hoppe@gmail.com>
2015-10-26 23:10:58 -0400

    Add redis to server config directions

--- configuration/configure_server.sh
+++ configuration/configure_server.sh
@@ -3,13 +3,15 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-
 # Grab basic python deps
 apt-get update
 apt-get install -y python-dev git python-pip dtach
 
 # Install SSL lib to remove pip warnings
 apt-get install -y libffi-dev libssl-dev
+
+# Install redis and celery
+sudo apt-get install -y redis-server
 
 # Install docker PGP key
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
